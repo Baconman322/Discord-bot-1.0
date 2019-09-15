fc95a1344646ce7b8bdd3bef533422ec0192ef06
Baconman322 <baconman322@gmail.com>
2019-09-15 09:10:33 -0400

    added 8 ball
    8 ball kida works

--- Dis bot.py
+++ Dis bot.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -17,5 +18,47 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} has left the server.')
 
+@Client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(Client.latency * 1000)}ms')
 
-Client.run('NjIyNjA3NDM1MTYwNjE2OTYw.XX4VGQ.00j805jDFJQqlM__-GCoU4IcJwk')
+
+
+@Client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = ['Yes'
+                 'No'
+                 'Maybe'
+                 'I dont care, figure it out your self'
+                 'It is certain.'
+                 'It is decidedly so.'
+                 'Without a doubt.'
+                 'Yes - definitely.'
+                 'You may rely on it.'
+                 'As I see it, yes.'
+                 'Most likely.'
+                 'Outlook good.'
+                 'Yes.'
+                 'Signs point to yes.'
+                 'Reply hazy, try again.'
+                 'Ask again later.'
+                 'Better not tell you now.'
+                 'Cannot predict now.'
+                 'Concentrate and ask again.'
+                 'Don\'t count on it.'
+                 'My reply is no.'
+                 'My sources say no.'
+                 'Outlook not so good.'
+                 'Very doubtful.' ]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+
+@Client.event():
+async def Tesla_sucks(ctx):
+    await ctx.send(f'shut up peasant.')
+
+
+
+
+
+Client.run('')
