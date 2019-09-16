dabbacc2f2b74afeb205e7b8beb15d9f4e39115c
Baconman322 <baconman322@gmail.com>
2019-09-15 20:19:57 -0400

    added mute

--- Dis bot.py
+++ Dis bot.py
@@ -115,6 +115,16 @@ async def hello(ctx):
 
 # Anti Spam
 
+@Client.command(pass_context = True)
+async def mute(ctx, member: discord.Member):
+     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
+        role = discord.utils.get(member.server.roles, name='Muted')
+        await bot.add_roles(member, role)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+        await bot.say(embed=embed)
+     else:
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+        await bot.say(embed=embed)
 
 
-Client.run('')
+Client.run('NjIyNjA3NDM1MTYwNjE2OTYw.XX6O1A.FmJTmeHHWCDOApMBBYX0CbP6Bo8')
